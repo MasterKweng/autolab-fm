f3970e85867a0c3233d40ad8e7c94b945923b42e
YuKwengRu <masterkweng@163.com>
2020-11-24 20:50:46 +0800

    修复bug

--- auto/plan/DatabaseTest/test_autolab.py
+++ auto/plan/DatabaseTest/test_autolab.py
@@ -9,7 +9,7 @@ from auto.rescources.DatabaseTest.conf import AUTOLAB
 def test_autolab_tables(database_connector):
     db = database_connector
 
-    res = db.has_tables(AUTOLAB["table"])
+    res = db.has_tables(AUTOLAB["tables"])
 
     for table in AUTOLAB["tables"]:
         print("table %s in database!" % table)
