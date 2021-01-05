35d9c4ee7cb1772624c0f3199108239290d62788
DESKTOP-3P00TCP\jsj <810260819>
2021-01-05 11:03:48 +0800

    钉钉通知调试

--- utils/notify.py
+++ utils/notify.py
@@ -10,7 +10,6 @@ from conf.conf import DINGTALK_URL
 
 # 发送钉钉文本通知
 def send_dingtalk_msg(msg):
-    print("2")
     headers = {"Content-Type": "application/json"}
     data = {
         "msgtype": "text",
@@ -18,10 +17,4 @@ def send_dingtalk_msg(msg):
             "content": msg
         }
     }
-    print("3")
     res=requests.post(DINGTALK_URL,data=json.dumps(data),headers=headers)
-    print("4")
-
-    print(res.text)
-
-# send_dingtalk_msg("123")
