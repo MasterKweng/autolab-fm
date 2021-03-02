248a3d35df9125f0dfe411939a2f60ed96554e48
yukwengru <masterkweng@163.com>
2021-03-02 17:10:42 +0800

    修改pipeline为win环境

--- auto/pipeline/DatabaseTest/test_autolab_win.groovy
+++ auto/pipeline/DatabaseTest/test_autolab_win.groovy
@@ -6,13 +6,15 @@ node {
     // init Python 运行环境
     // 初始化一些运行态需要的全局变量
     stage("Init"){
-        // echo "Init Run Env"
+        @echo on
+        echo "Init Run Env"
         bat 'python3 --version'
     }
 
     // 从git获取最新代码
     stage("Check out"){
-        // echo "check out last code"
+        @echo on
+        echo "check out last code"
         checkout scm
     }
 
